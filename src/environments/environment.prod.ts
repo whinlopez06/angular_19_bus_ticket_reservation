0bdf2f160f03b2ca821d582d95cd5ff6c358a7d5
whinlo <whinlopez06@gmail.com>
2025-12-12 13:55:07 +0800

    change render API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://laravel12-busbooking-5.onrender.com/api/'
+  apiUrl: 'https://laravel-12-bus-ticket-reservation.onrender.com/api/'
 }
